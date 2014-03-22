c70df19371a313b1b3a39d115980275075c3dd96
frantisan <frantisaul@gmail.com>
2014-03-22 09:31:00 -0500

    terminado

---
+++ EjercicioNavController/Flujo2ViewController.h
@@ -0,0 +1,14 @@
+//
+//  Flujo2ViewController.h
+//  EjercicioNavController
+//
+//  Created by Franti Saúl Huamán Mera on 3/15/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Flujo2ViewController : UIViewController
+- (IBAction)cerrarButton:(id)sender;
+
+@end
